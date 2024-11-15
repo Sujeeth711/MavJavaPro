3267a12c18b1185bcd531784fa58d3fac5673346
Sujeeth711 <sujeeth5711@gmail.com>
2024-11-15 19:11:27 +0530

    added files

---
+++ src/main/java/com/project/java/MavenProjOne/App.java
@@ -0,0 +1,13 @@
+package com.project.java.MavenProjOne;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Sujeeth 22BD1A0525" );
+    }
+}
